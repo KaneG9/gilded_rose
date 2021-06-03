4f588ed873419d2f0c815b01e5f36d3fe9976d2b
Kane <kgin888@gmail.com>
2021-06-03 14:04:13 +0100

    added update backstage pass and standard item

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,4 +1,5 @@
 require_relative 'item.rb'
+require_relative 'quality_manager.rb'
 
 class GildedRose
 
@@ -11,36 +12,18 @@ class GildedRose
       next if item.name == "Sulfuras, Hand of Ragnaros"
       case item.name
       when "Aged Brie"
-        item.quality += 1
+        if item.quality < 50
+          item.quality += 1
+        end
         if item.sell_in <= 0
           if item.quality < 50
             item.quality += 1
           end
         end
       when "Backstage passes to a TAFKAL80ETC concert"
-        item.quality += 1
-        if item.sell_in < 11
-          if item.quality < 50
-            item.quality += 1
-          end
-        end
-        if item.sell_in < 6
-          if item.quality < 50
-            item.quality += 1
-          end
-        end
-        if item.sell_in <= 0
-          item.quality = 0
-        end
+        QualityManager.update_backstage_pass(item)
       else
-        if item.quality > 0
-          item.quality -= 1
-        end
-        if item.sell_in <= 0
-          if item.quality > 0
-            item.quality -= 1
-          end
-        end
+        QualityManager.update_standard_item(item)
       end
       item.sell_in -= 1
     end
--- lib/quality_manager.rb
+++ lib/quality_manager.rb
@@ -1,6 +1,36 @@
 class QualityManager
+
+  STANDARD_DEGREDATION = 1
+  MINIMUM_QUALITY = 0
+  EXPIRED_LIMIT = 0
+
   def self.update_standard_item(item)
-    item.quality -= 1
-    item.sell_in -= 1
+    if expired?(item)
+      item.quality -= 2 * STANDARD_DEGREDATION
+    else
+      item.quality -= STANDARD_DEGREDATION
+    end 
+    item.quality = MINIMUM_QUALITY if below_minimum_quality?(item)
+  end
+
+  def self.update_backstage_pass(item)
+    return item.quality = MINIMUM_QUALITY if expired?(item)
+    if item.sell_in < 6 
+      item.quality += 3 * STANDARD_DEGREDATION
+    elsif item.sell_in < 11
+      item.quality += 2 * STANDARD_DEGREDATION
+    else
+      item.quality += STANDARD_DEGREDATION
+    end
+  end
+
+  private
+
+  def self.below_minimum_quality?(item)
+    item.quality < MINIMUM_QUALITY
+  end
+
+  def self.expired?(item)
+    item.sell_in <= EXPIRED_LIMIT
   end
 end
